65be7fb00a33d2726075be56b64a4831d6eaf625
Ramiro Lupo <luporamiro5@gmail.com>
2022-05-17 09:57:37 -0300

    Desafío: Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <h1> PadelMania</h1>
+      <h1> Pádel Manía</h1>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import './NavBar.css'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 
@@ -12,10 +11,25 @@ const NavBar = () => {
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            PadelMania
-                        </Typography>
-                        <Button color="inherit">Login</Button>
+                        <div className='container-logo'>
+                            {// eslint-disable-next-line jsx-a11y/alt-text
+                            }<img src='./logo.jpg' /> 
+                        </div>
+                        <ul>
+                            <li>
+                                <button>Paletas</button>
+                            </li>
+                            <li>
+                                <button>Accesorios</button>
+                            </li>
+                            <li>
+                                <button>Indumentaria</button>
+                            </li>
+                            <li>
+                                <button>Bolsos</button>
+                            </li>
+                        </ul>
+                        <Button color="inherit">Iniciar Sesión</Button>
                     </Toolbar>
                 </AppBar>
             </Box>
