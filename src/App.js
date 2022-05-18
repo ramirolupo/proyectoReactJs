928381e0b6ee00c807347418e88fa1f498232cbe
Ramiro Lupo <luporamiro5@gmail.com>
2022-05-18 18:44:40 -0300

    Desafío: Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-//import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import Card from './components/Card/Card'
+import CardItem from './components/Card/Card'
+import {Container, Grid} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <h1> Pádel Manía</h1>
-      <Card/>
+      <Container>
+        <Grid container className='general-container'>
+          <Grid item md={3}>
+            <CardItem title={"BullPadel Hack 03"} price={95000}/>
+          </Grid>
+          <Grid item md={3}>
+            <CardItem title={"Adidas Metalbone 3.1"} price={110000}/>
+          </Grid>
+          <Grid item md={3}>
+            <CardItem title={"Head Delta Pro"} price={83000}/>
+          </Grid>
+          <Grid item md={3}>
+            <CardItem title={"NOX AT10 Genius"} price={85000}/>
+          </Grid>
+        </Grid>
+      </Container>
+      
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,38 @@
-const Card = () => {
+import './Card.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Button }  from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#435b63',
+            main: '#14323d',
+            dark: '#0e232a',
+            contrastText: '#fff',
+        },
+    },
+});
+
+const CardItem = (props) => {
     return(
-        <div>
-            CARD
-        </div>
+        <ThemeProvider theme={theme}>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                <div className="card-item">
+                    <div>
+                        {// eslint-disable-next-line jsx-a11y/alt-text
+                        }<img src="F-Bullpadel Hack 03.jpg"/>
+                    </div>
+                    <p>Titulo</p>
+                    <span>Precio</span>
+                    <Button variant={'contained'} >Holiis</Button>
+                </div>
+                </CardContent>
+            </Card>
+        </ThemeProvider>
     )
 }
 
-export default Card
+export default CardItem
