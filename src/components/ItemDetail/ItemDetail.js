e00ea1c33c7bc84e20fbeff0f098ae4d07a90af6
Ramiro Lupo <luporamiro5@gmail.com>
2022-05-31 20:15:04 -0300

    Add DetailPage to Proyect

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,12 +1,36 @@
+import './ItemDetail.css'
+
+
 const ItemDetail = ( {data} ) =>{
     return(
         <>
-            <div>
-                <img src={`./${data.image}`} alt='imgDetail'/>
-            </div>
-            <p>{data.title}</p>
-            <h2>{data.price}</h2>
-        </>
+            <div className='detail-product-container'>
+                <div className="detail-product-image">
+                    <img src={`/${data.image}`} alt="imgProduct"/>
+                </div>
+                <div className="detail-product-info">
+                    <div className='detail-product-info__title'>
+                        <h2>{data.title}</h2>
+                    </div>
+                    <p>$ {data.price}</p>
+                    <span className='price'>8 Cuotas sin interés de $ {data.price / 8}</span>
+                    <p>Una pala increíble ahora con <span>Fibra de Carbono 18K</span></p>
+                    <p>Incluye el nuevo sistema de cordón de seguridad.</p>
+                    <h2>Detalles de la pala</h2>
+                    <ul className='product-details'>
+                        <li><span>Potencia:</span> 10/10</li>
+                        <li><span>Control:</span> 9/10</li>
+                        <li><span>Peso:</span> 360 – 375 g</li>
+                        <li><span>Perfil del marco:</span> 38 mm</li>
+                        <li><span>Forma de pala:</span> Gota</li>
+                        <li><span>Marco:</span> 100% carbono</li>
+                        <li><span>Goma:</span> HR3</li>
+                        <li><span>Cara:</span> Fiber Carbon 18K</li>
+                        <li><span>Smartstrap</span></li>
+                    </ul>
+                </div>  
+            </div>  
+        </> 
     )
 }
 
