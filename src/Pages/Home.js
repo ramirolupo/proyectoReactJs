5f834ae2c281060e0e8e7e0a68d1facfff15558b
Ramiro Lupo <luporamiro5@gmail.com>
2022-06-04 15:19:22 -0300

    pruebita

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,31 @@
- import CardList from '../components/CardList/CardList'
- import { useState, useEffect } from 'react'
- import productos from '../utils/productsMock'
+import CardList from '../components/CardList/CardList'
+import { useState, useEffect } from 'react'
+import productos from '../utils/productsMock'
 
 const Home = () =>{
-     const [products, setProducts] = useState([])
-    
+    const [products, setProducts] = useState([])
 
-     const getProducts = () =>{
-         return new Promise( (resolve, reject) => {
-            //setTimeout( () => {
-                 resolve(productos)
-             //}, 3000)
-         })
-     }
 
-     useEffect( () => {
-         getProducts()
-         .then( (res) => {
-             setProducts(res)
-         })
-     }, [])
+    const getProducts = () =>{
+        return new Promise( (resolve, reject) => {
+        //setTimeout( () => {
+                resolve(productos)
+            //}, 3000)
+        })
+    }
+
+    useEffect( () => {
+        getProducts()
+        .then( (res) => {
+            setProducts(res)
+        })
+    }, [])
 
     
     return(
             <div>
                 <h2>HOME</h2>
-                { <CardList title={'PRODUCTOS RECOMENDADOS'} products={products}/>}
+                { <CardList title={'Productos Recomendados'} products={products}/>}
             </div>
     )
 }
